35aa7c98420fd07ce49fbb03f7bfb91edc2c5abd
mathisDlmr <mathis.dlmr@gmail.com>
2025-02-15 21:16:39 +0100

    Compta Heures EmployedTutor

--- app/Filament/Resources/ComptabiliteHeuresResource.php
+++ app/Filament/Resources/ComptabiliteHeuresResource.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\Admin\ComptabiliteHeuresResource\Pages;
 use App\Models\User;
 use App\Models\Creneaux;
+use App\Models\EmployedTutorList;
 use App\Enums\Roles;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -35,14 +38,36 @@ class ComptabiliteHeuresResource extends Resource
                 TextColumn::make('heures_total')
                     ->label('Heures totales')
                     ->getStateUsing(fn(User $record) => 
-                        Creneaux::where('tutor1_id', $record->id)
-                            ->orWhere('tutor2_id', $record->id)
-                            ->sum(DB::raw("TIMESTAMPDIFF(HOUR, start, end)"))
-                    ),
+                    Creneaux::where('tutor1_id', $record->id)
+                        ->orWhere('tutor2_id', $record->id)
+                        ->sum(DB::raw("(julianday(end) - julianday(start)) * 24"))
+                ),
             ])
-            ->filters([])
+            ->filters([
+                Filter::make('semaine')
+                    ->form([
+                        DatePicker::make('start_date')->label('Début'),
+                        DatePicker::make('end_date')->label('Fin'),
+                    ])
+                    ->query(function ($query, array $data) {
+                        if (!empty($data['start_date']) && !empty($data['end_date'])) {
+                            return $query->whereHas('creneaux', function ($q) use ($data) {
+                                $q->whereBetween('start', [$data['start_date'], $data['end_date']]);
+                            });
+                        }
+                        return $query;
+                    }),
+            ])     
+            ->modifyQueryUsing(fn ($query) => 
+                $query->whereIn('email', EmployedTutorList::pluck('email'))
+            )     
             ->actions([])
             ->bulkActions([]);
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
     }
 
     public static function getRelations(): array
